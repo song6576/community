182ea4951b49571bc09c36e31c5a05c23641b5f6
幻城 <1695444030@qq.com>
2021-11-11 21:29:15 +0800

    物资信息

---
+++ src/pages/supplies/materialInformation/materialInformation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MaterialInformation extends Component {
+  render() {
+    return (
+      <div>
+        物资信息
+      </div>
+    )
+  }
+}
