c3c1a42a66f01fbc8d4201eedc403ac601d54236
幻城 <1695444030@qq.com>
2021-11-11 21:28:00 +0800

    病例统计

---
+++ src/pages/cases/cases.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+/**
+ * 病例统计
+ */
+export default class Cases extends Component {
+  render() {
+    return (
+      <div>
+        病例统计
+      </div>
+    )
+  }
+}
