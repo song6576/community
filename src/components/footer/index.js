8170e346342cd423de8fba563b8d7dc0f61ab0a7
幻城 <1695444030@qq.com>
2021-11-11 21:27:14 +0800

    底部模块

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './index.less'
+
+/**
+ * 这是底部组件
+ */
+export default class Footerd extends Component {
+  render() {
+    return (
+      <div className='footerd'>
+        <span>@ 小区新冠防疫系统</span>
+      </div>
+    )
+  }
+}
