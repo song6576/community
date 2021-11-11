e042314b3bcba5587a94552a87acb495a1ee3b58
幻城 <1695444030@qq.com>
2021-11-11 21:29:59 +0800

    访客管理

---
+++ src/pages/visitors/visitors.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+/**
+ * 访客管理
+ */
+export default class Visitors extends Component {
+  render() {
+    return (
+      <div>
+        访客管理
+      </div>
+    )
+  }
+}
