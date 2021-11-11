cfe894a42719fa2a2be41dbb22aa3e2294671783
幻城 <1695444030@qq.com>
2021-11-11 21:28:25 +0800

    健康上报

---
+++ src/pages/health/health.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+/**
+ * 健康上报
+ */
+export default class Health extends Component {
+  render() {
+    return (
+      <div>
+        健康上报
+      </div>
+    )
+  }
+}
